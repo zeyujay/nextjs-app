dea3c9ae8f5a0547a23744f6fba7c84b8b230864
zeyujay <zeyujay@gmail.com>
2023-04-11 15:56:08 +0800

    123

--- src/app/goal/page.tsx
+++ src/app/goal/page.tsx
@@ -2,7 +2,7 @@
  * @Author: zeyujay zeyujay@gmail.com
  * @Date: 2023-04-10 17:36:44
  * @LastEditors: zeyujay zeyujay@gmail.com
- * @LastEditTime: 2023-04-11 02:56:28
+ * @LastEditTime: 2023-04-11 15:55:30
  * @FilePath: /note/Users/zeyu/Documents/work/next-notion/src/app/goal/page.tsx
  * @Description:
  *
@@ -13,7 +13,7 @@ import Time from "./component/Time";
 import styles from "./page.module.css";
 import React from "react";
 async function Goal() {
-  const result = await fetch("http://localhost:3002/test/goal", {
+  const result = await fetch("https://zeyujay.xyz:3002/test/goal", {
     cache: "no-store",
     method: "GET",
   });
